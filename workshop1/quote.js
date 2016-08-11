e029762505ebca38a6342cd791b793df860b5673
EboniCG <eg5675@bard.edu>
2016-08-11 12:12:05 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -4,7 +4,7 @@
  * L&T quote generator
  */
 
-var things = ["Music", "Art", "Dance", "Theater", "Arithmetic", "Architecture"];
+var things = ["Music", "Art", "Move", "Acting", "Arithmetic", "Building"];
 var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
